ec2715e4d6e04d9a36e0b443625f23b34473ba76
BotanicalCoder <x3mace007@gmail.com>
2023-09-21 17:37:12 +0100

    feat: add tnc and user manual pages

---
+++ pages/tnc.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <template v-slot:header>
+        <!-- content for the header slot -->
+        <div class="flex items-center">
+          <Icon
+            name="material-symbols:arrow-back-ios"
+            size="20"
+            color="white"
+            @click="goBack"
+            class="cursor-pointer"
+          />
+          <p class="text-white text-xl font-bold text-center w-full">
+            Terms & Conditions
+          </p>
+        </div>
+      </template>
+
+      <div class="mt-[5rem] flex items-center justify-center">
+        <h2 class="text-4xl"> Coming soon </h2>
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+const goBack = () => navigateTo("/");
+</script>
+
+<style scoped></style>
