934661930786237a6c378c2be162b7ec9f9147a7
BotanicalCoder <x3mace007@gmail.com>
2023-12-07 18:43:54 +0100

    bugfix: add min date as today

--- components/input/daypicker.vue
+++ components/input/daypicker.vue
@@ -56,6 +56,7 @@ const handleChange = (e: any) => {
         hide-input-icon
         auto-apply
         disable-month-year-select
+        :min-date="new Date(Date.now())"
       />
 
       <!-- <component :is="CalendarIcon" class="w-5 h-5 text-gray-500" /> -->
